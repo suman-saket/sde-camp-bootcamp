87dca6497371c0111cbbf4ebd4b2e09640c8ee79
Saket Suman <sumansaket7@gmail.com>
2022-02-05 09:59:55 +0530

    Few chnages in code

--- Day3/reverseString.cpp
+++ Day3/reverseString.cpp
@@ -2,17 +2,20 @@
 
 using namespace std;
 
-void reverseString(int arr[], int s, int e)
+int reverseString(int arr[], int s, int e)
 {
 
-    if (s >= e)
-        return;
+    int temp;
+    if (s < e)
+    {
 
-    int temp = arr[s];
-    arr[s] = arr[e];
-    arr[e] = temp;
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
 
-    reverseString(arr, s + 1, e - 1);
+        reverseString(arr, s + 1, e - 1);
+    }
+    return 0;
 }
 
 int main()
@@ -20,11 +23,16 @@ int main()
     int n;
     cin >> n;
     int arr[n];
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-
     reverseString(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+    cout << endl;
+
     return 0;
 }
